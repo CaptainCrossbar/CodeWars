7e7976242d6070e77ac4badc491fa802f48da105
Peter Boyle <31522669+CaptainCrossbar@users.noreply.github.com>
2020-03-09 14:37:14 -0400

    Create (8) Square(n) Sum.py

---
+++ (8) Square(n) Sum.py
@@ -0,0 +1,11 @@
+def square_sum(numbers):
+    
+    #variable
+    sum = 0
+    
+    #loop to square and add the numbers
+    for x in numbers:
+        sum += x ** 2
+    
+    #returns answer
+    return sum
